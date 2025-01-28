945a2d686e78b8e609ee5cb8d4c00c2ecb1207e1
kunal latta <95677039+KUNALLATTA29@users.noreply.github.com>
2025-01-28 22:57:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const {Server} = require('socket.io')
-
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
